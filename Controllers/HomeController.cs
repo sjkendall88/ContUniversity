2b8b32e1fec58a5681bc7906ecf1f4f3c63f0ab7
sjkendall88 <sjkendall88@byui.edu>
2019-11-21 10:44:33 -0600

    completed Sort step

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,21 +5,44 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ContUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using ContUniversity.Data;
+using ContUniversity.Models.SchoolViewModels;
 
 namespace ContUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly SchoolContext _context;
+
+        public HomeController(SchoolContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
 
-            return View();
+        //    return View();
+        //}
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Students
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         public IActionResult Contact()
