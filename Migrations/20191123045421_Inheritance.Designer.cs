e8ab06816878ac3f32ded2f171e50c3bacb58c3f
sjkendall88 <sjkendall88@byui.edu>
2019-11-23 01:00:19 -0600

    Completed Inheritance

--- Migrations/20191123045421_Inheritance.Designer.cs
+++ Migrations/20191123045421_Inheritance.Designer.cs
@@ -4,14 +4,16 @@ using ContUniversity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ContUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20191123045421_Inheritance")]
+    partial class Inheritance
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
